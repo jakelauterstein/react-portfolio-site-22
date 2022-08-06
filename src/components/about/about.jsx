25028a0d1700dc4b7ad5e115a7a2d16e211005d5
Jake Lauterstein <97852239+jakelauterstein@users.noreply.github.com>
2022-08-05 20:38:48 -0500

    added copy

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -40,7 +40,8 @@ const about = () => {
           </article>
         </div>
 
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt delectus aspernatur placeat fugiat voluptate rem dicta? Aut quos consectetur harum saepe, obcaecati veniam ullam error ipsum tempore tenetur rerum expedita?</p>
+        <p>A little about myself: I'm a professioanl front-end web developer building visually engaging interfaces and attention-gaining applications. I received my training at the University of Texas at Austin's Full Stack Coding Bootcamp. I specialize in writing HTML, CSS, Javascript, React, and Wordpress, with an emphasis on responsive, mobile-friendly design. With each project, my goal is to create an engaging interface that creates a seamless user experience across all facets of the application. I'm currently working for White Hat Agency, a creative marketing company based in Austin, TX. 
+</p>
           
         <a href="#contact" className='btn btn-primary' >Let's Talk</a>
 
--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -18,23 +18,23 @@ const services = () => {
           <ul className="service__list">
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <p>Professional Adobe-based wireframes outline a top-level view of your project.</p>
             </li> 
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <p>Branding and Adobe Illustrator logo creation makes your logo scalable from billboard to business card.</p>
             </li> 
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <p>Attention-gaining formats with optimized performance keep your users engaged on your page.</p>
             </li> 
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <p>High-quality photos, videos, and animations bring your page a cutting-edge, dynamic feel.</p>
             </li> 
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <p>Easy to navigate pages with custom icons allow users to easily traverse your site and explore your offerings.</p>
             </li> 
           </ul>
         </article>
@@ -45,25 +45,29 @@ const services = () => {
           </div>
 
           <ul className="service__list">
-            <li>
+          <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <p>Search engine optimization keeps your website on the top tier of google results.</p>
             </li> 
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <p>Extensive modular components allow you to customize and edit your site with quick turnaround time.</p>
             </li> 
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <p>Database integration keeps your users personal data secure and your record-keeping stress-free</p>
             </li> 
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <p>Connect your site with large-scale applications to leverage data from all over the web.</p>
             </li> 
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <p>Cutting-edge plugins create high-level functionality with minimal overhead.</p>
+            </li> 
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Continual support keeps your website and applications updated for all of your future developments.</p>
             </li> 
           </ul>
         </article>
@@ -76,23 +80,23 @@ const services = () => {
           <ul className="service__list">
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <p>Brand-curated media assets bring a cohesive feel to your social media platforms.</p>
             </li> 
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <p>Email marketing campaigns keep your users in-the-know about all of your latest offerings.</p>
             </li> 
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <p>Social media optimization keeps your business trending on all of the major apps.</p>
             </li> 
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <p>Viral marketing campaigns lead to optimized cross-platform lead generation.</p>
             </li> 
             <li>
               <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <p>Merchandise assistance and event coordination promotes brand awareness in your community and abroad.</p>
             </li> 
           </ul>
         </article>
